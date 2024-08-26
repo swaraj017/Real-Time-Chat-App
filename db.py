0517db82c33240d499d0261cba38d5f716191b55
swaraj017 <pyush671@gamil.com>
2024-08-26 15:04:59 +0530

    Initial commit

---
+++ db.py
@@ -0,0 +1,68 @@
+from pymongo import MongoClient, errors
+from werkzeug.security import generate_password_hash
+from user import User
+import hashlib
+from datetime import datetime
+
+# Connect to MongoDB
+client = MongoClient("mongodb+srv://swaraj:1991@chattapp.jrylb.mongodb.net/?retryWrites=true&w=majority&appName=ChattApp")
+chat_db = client.get_database("ChatDb")
+
+# Collections
+users_collection = chat_db.get_collection("users")
+roommembers = chat_db.get_collection("roommembers")
+messages_collection = chat_db.get_collection("messages")
+
+def save_user(username, email, password):
+    password_hash = generate_password_hash(password)
+    users_collection.insert_one({'_id': username, 'email': email, 'password': password_hash})
+
+def hash_room_id(room_id):
+    if room_id is None:
+        raise ValueError("Room ID cannot be None")
+
+    room_id_bytes = room_id.encode('utf-8')
+    sha256 = hashlib.sha256()
+    sha256.update(room_id_bytes)
+    return sha256.hexdigest()
+
+def save_room(room_id, username):
+    hashed_id = hash_room_id(room_id)
+    existing_member = roommembers.find_one({'roomid': hashed_id, 'username': username})
+
+    if not existing_member:
+        try:
+            roommembers.insert_one({'roomid': hashed_id, 'username': username})
+        except errors.DuplicateKeyError:
+            print(f"Duplicate entry error: User '{username}' already exists in the room.")
+
+def get_room_members(room_id):
+    hashed_id = hash_room_id(room_id)
+    members_cursor = roommembers.find({'roomid': hashed_id})
+    members = set(member['username'] for member in members_cursor)
+    return list(members)
+
+def save_message(room_id, username, message):
+    messages_collection.insert_one({
+        'roomid': hash_room_id(room_id),
+        'username': username,
+        'message': message,
+        'timestamp': datetime.utcnow()
+    })
+
+def get_messages(room_id):
+    hashed_id = hash_room_id(room_id)
+    messages_cursor = messages_collection.find({'roomid': hashed_id}).sort('timestamp', 1)
+    messages = [{'username': msg['username'], 'message': msg['message']} for msg in messages_cursor]
+    return messages
+
+def delete_room(room_id):
+    hashed_id = hash_room_id(room_id)
+    roommembers.delete_many({'roomid': hashed_id})
+    messages_collection.delete_many({'roomid': hashed_id})
+
+
+
+def get_user(username):
+    user_data = users_collection.find_one({'_id': username})
+    return User(user_data['_id'], user_data['email'], user_data['password']) if user_data else None
---
+++ main.py
@@ -0,0 +1,128 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_socketio import SocketIO, join_room, leave_room, emit
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from pymongo.errors import DuplicateKeyError
+from db import get_user, save_user, save_room, get_room_members, save_message, get_messages, delete_room
+
+app = Flask(__name__)
+app.secret_key = "My Chatt App"
+
+socketio = SocketIO(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    message = ""
+    if request.method == "POST":
+        username = request.form.get('username')
+        password_input = request.form.get('password')
+        user = get_user(username)
+        if user and user.check_password(password_input):
+            login_user(user)
+            return redirect(url_for("home"))
+        else:
+            message = "Failed to Login"
+    return render_template('login.html', message=message)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    message = ''
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        try:
+            save_user(username, email, password)
+            return redirect(url_for('login'))
+        except DuplicateKeyError:
+            message = "User already exists!"
+    return render_template('signup.html', message=message)
+
+@app.route('/chatt')
+@login_required
+def chat():
+    username = request.args.get('username')
+    room = request.args.get('room')
+    if username and room:
+        # Save user to room if not already present
+        save_room(room, username)
+
+        # Get room members
+        members = get_room_members(room)
+
+        # Get existing messages
+        messages = get_messages(room)
+
+        # Render the chat page with the updated list of members and existing messages
+        return render_template("chat.html", username=username, room=room, members=members, messages=messages)
+    else:
+        # Handle the case where username or room is not provided
+        return redirect(url_for("home"))
+
+@app.route('/delete_room/<room>', methods=['POST'])
+@login_required
+def delete_chat(room):
+    # Check if the user is authorized to delete the room (implement your own logic here)
+    if current_user.is_authenticated:
+        delete_room(room)
+        return redirect(url_for('home'))
+    else:
+        return redirect(url_for('login'))
+
+@socketio.on('join_room')
+def handle_join_room_event(data):
+    username = data['username']
+    room = data['room']
+
+    app.logger.info("{} has joined the room {}".format(username, room))
+    join_room(room)
+    socketio.emit('join_room_announcement', data, room=room)
+
+@socketio.on('leave_room')
+def handle_leave_room_event(data):
+    username = data['username']
+    room = data['room']
+
+    app.logger.info("{} has left the room {}".format(username, room))
+    leave_room(room)
+    socketio.emit('leave_room_announcement', data, room=room)
+
+@socketio.on('send_message')
+def handle_send_message(data):
+    room_id = data['room']
+    username = data['username']
+    message = data['message']
+
+    # Save message to database
+    save_message(room_id, username, message)
+
+    app.logger.info(f"{username} has sent a message to room {room_id}")
+    emit('receive_mess', {
+        'username': username,
+        'message': message
+    }, room=room_id)
+
+@login_manager.user_loader
+def load_user(username):
+    return get_user(username)
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
